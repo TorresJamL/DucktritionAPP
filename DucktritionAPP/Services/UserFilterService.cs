48ba54f5f99fb806dc8a72a857a1c32c01844c43
Jamil Torres <torresjamil008@gmail.com>
2025-11-02 02:15:45 -0500

    Implemented a barebones filter service class. Known issues: Android Emulator will not run. |- This might be because of how hefty the program is becoming, but more likelily I did something wrong.
    TODO: fix emulation issue, test on real device, implement rest of views. purely in looks, functionality not as required (yet)

---
+++ DucktritionAPP/Services/UserFilterService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DucktritionAPP.Services
+{
+    public class UserFilterService
+    {
+        private static UserFilterService _inst;
+        public static UserFilterService Inst => _inst ??= (_inst = new UserFilterService());
+
+        public List<string> UserFilters { get; private set; } = new();
+        public event Action UserFilterChanged;
+
+        public void AddFilter(string filter)
+        {
+            if (!UserFilters.Contains(filter))
+            {
+                UserFilters.Add(filter);
+                UserFilterChanged?.Invoke();
+            }
+        }
+        public void RemoveFilter(string filter)
+        {
+            if (!UserFilters.Contains(filter))
+            {
+                UserFilters.Remove(filter);
+                UserFilterChanged?.Invoke();
+            }
+        }
+    }
+}
--- DucktritionAPP/Views/HomePage.xaml.cs
+++ DucktritionAPP/Views/HomePage.xaml.cs
@@ -1,9 +1,102 @@
+using DucktritionAPP.Services;
+
 namespace DucktritionAPP.Views;
 
 public partial class HomePage : ContentPage
 {
+	List<string> appFilters = new() { "Vegetarian", "Almond Allegy", "Shellfish Allergy", "Lactose Intolerant" };
+    UserFilterService filterService = UserFilterService.Inst;
+
 	public HomePage()
 	{
 		InitializeComponent();
+        BuildAvailableFilters();
 	}
+    void BuildAvailableFilters()
+    {
+        AvailableFiltersArea.Children.Clear();
+
+        foreach (var filter in appFilters.Except(filterService.UserFilters))
+        {
+            var layout = new HorizontalStackLayout
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                Spacing = 10
+            };
+
+            var label = new Label
+            {
+                Text = filter,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            var addButton = new Button
+            {
+                Text = "+",
+                HorizontalOptions = LayoutOptions.End,
+                WidthRequest = 30,
+                HeightRequest = 30,
+                CornerRadius = 15,
+                BackgroundColor = Colors.LightGray
+            };
+
+            addButton.Clicked += (s, e) =>
+            {
+                filterService.UserFilters.Add(filter);
+                BuildActiveFilters();
+                BuildAvailableFilters();
+            };
+
+            layout.Children.Add(label);
+            layout.Children.Add(addButton);
+            AvailableFiltersArea.Children.Add(layout);
+        }
+    }
+
+    void BuildActiveFilters()
+    {
+        ActiveFiltersArea.Children.Clear();
+
+        var header = new Label
+        {
+            Text = "Active Filters",
+            FontAttributes = FontAttributes.Bold
+        };
+        ActiveFiltersArea.Children.Add(header);
+
+        foreach (var filter in filterService.UserFilters)
+        {
+            var layout = new HorizontalStackLayout
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                Spacing = 10
+            };
+
+            var label = new Label
+            {
+                Text = filter,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            var removeButton = new Button
+            {
+                Text = "X",
+                WidthRequest = 30,
+                HeightRequest = 30,
+                CornerRadius = 15,
+                BackgroundColor = Colors.LightGray
+            };
+
+            removeButton.Clicked += (s, e) =>
+            {
+                filterService.UserFilters.Remove(filter);
+                BuildActiveFilters();
+                BuildAvailableFilters();
+            };
+
+            layout.Children.Add(label);
+            layout.Children.Add(removeButton);
+            ActiveFiltersArea.Children.Add(layout);
+        }
+    }
 }
